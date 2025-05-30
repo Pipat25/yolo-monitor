3cf61935c3db48f7518aedd8f8b0bd9480e5ace4
Pipat25 <161411458+Pipat25@users.noreply.github.com>
2025-05-30 11:28:57 +0700

    Add initial project files

---
+++ update_csv.py
@@ -0,0 +1,29 @@
+import os
+import time
+import requests
+
+SOURCE_URL = " https://6f10-27-55-95-181.ngrok-free.app/results.csv"  # แทนด้วย URL จาก ngrok
+FILE_PATH = "results.csv"
+
+
+def update_csv():
+    try:
+        response = requests.get(SOURCE_URL, timeout=5)
+        if response.status_code == 200:
+            with open(FILE_PATH, 'wb') as f:
+                f.write(response.content)
+            print(
+                f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] อัปเดต results.csv สำเร็จ"
+            )
+        else:
+            print(
+                f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] เกิดข้อผิดพลาด: {response.status_code}"
+            )
+    except Exception as e:
+        print(
+            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] เกิดข้อผิดพลาดในการดึงไฟล์: {e}"
+        )
+
+
+if __name__ == "__main__":
+    update_csv()
